eb7257626c6bbe29083b1bb51a9fbed053f7c365
Sellin Gauthier <gsellin@oui.sncf>
2018-07-26 11:30:43 +0200

    improve weather url

--- weather/WeatherApi.swift
+++ weather/WeatherApi.swift
@@ -12,13 +12,20 @@ import PromiseKit
 
 public class WeatherApi {
     
-    private let weatherUrl = "https://api.openweathermap.org/data/2.5/forecast?id=6455259&appid=191fd4d1087569b2e5ac9e98043e8402&lang=fr&units=metric"
+    private let baseUrl = "https://api.openweathermap.org/data/2.5/"
+    private let forecasteUrl = "forecast"
+    private let cityId = "?id=6455259"
+    private let appId = "&appid=191fd4d1087569b2e5ac9e98043e8402"
+    private let params = "&lang=fr&units=metric"
     
     public func getWeather() -> Promise<WeatherForecast> {
         return Promise<WeatherForecast> { fulfill, reject in
-            
+            guard let weatherUrl = URL(string: baseUrl + forecasteUrl + cityId + appId + params) else {
+                reject(WeatherApi.ApiError.invalidUrl)
+                return
+            }
             // Performing an Alamofire request to get the data from the URL
-            Alamofire.request(self.weatherUrl).responseJSON { response in
+            Alamofire.request(weatherUrl).responseJSON { response in
                 switch response.result {
                 case .success:
                     // the response data to parse
@@ -29,15 +36,21 @@ public class WeatherApi {
                             let myStruct = try decoder.decode(WeatherForecast.self, from: json)
                             fulfill(myStruct)
                         } catch let err {
+                            print("invalide json")
                             reject(err)
                         }
                     } else {
                         print("Erreur : pas de données")
+                        reject(WeatherApi.ApiError.weatherNoData)
                     }
                 case .failure(let error):
                     reject(error)
                 }
             }
         }
+    }
+    
+    enum ApiError: Error {
+        case invalidUrl, weatherNoData
     }
 }
